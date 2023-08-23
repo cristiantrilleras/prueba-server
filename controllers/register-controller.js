4aa65b86eeadffb18fe24c06983a12020a3ca81f
cristrilleras <cris_01lds@yahoo.es>
2023-08-23 08:41:10 -0500

    prueba server1

--- controllers/register-controller.js
+++ controllers/register-controller.js
@@ -1,12 +1,34 @@
 const bcrypt = require('bcryptjs');
+let mysql = require('mysql')
+
+const pool = mysql.createPool({
+  connectionLimit : 100,
+  host     : 'localhost',
+  user     : 'root',
+  password : '',
+  database : 'bd_persona',
+  debug    :  false
+});
 
 
 let register = (req,res) => {
-    let email = req.body.email;
-    let password = req.body.password;
-    let name = req.body.name;
-    let lastName = req.body.last_name;
-    console.log(email, password, name, lastName);
+    let nombre = req.body.nombre;
+    let id = req.body.id;
+    let telefono = req.body.telefono;
+    let edad = req.body.edad;
+    let profesion = req.body.profesion;
+
+    let insertQuery = 'INSERT INTO persona (id,nombre, edad, telefono, profesion) VALUES (?,?,?,?,?)';
+    let query = mysql.format(insertQuery,[id,nombre,edad,telefono,profesion]);
+    pool.query(query,(err, response) => {
+        if(err) {
+            console.error(err);
+            return;
+        }
+        console.log(response.insertId);
+    });
+    
+
     return res.status(201).send(
       { status: 'register ok'}
     );       
--- middleware/register-validator.js
+++ middleware/register-validator.js
@@ -2,11 +2,12 @@ const { check, validationResult } = require('express-validator');
 
 
 let validatorParams = [
-  check('email').isEmail(),
-  check('password').isLength({ min: 8, max: 15}),
-  check('name').isLength({ min: 1, max: 255}),
-  check('last_name').isLength({ min: 1, max: 255})
-];
+  check('id').isLength({min:0}),
+  check('nombre').isLength({ min: 4, max: 15}),
+  check('edad').isInt,
+  check('telefono').isInt({ min: 1, max: 50}),
+  check('profesion').isString
+ ];
    
 
 function validator(req, res, next) {
